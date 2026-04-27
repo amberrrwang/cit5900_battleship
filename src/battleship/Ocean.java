33ecdfde10fa155857cf85d96359d1ce5c010e75
kvaitohi <kvaitohi@penncareylaw.upenn.edu>
2026-04-26 23:15:09 -0400

    Update Ocean.java

--- src/battleship/Ocean.java
+++ src/battleship/Ocean.java
@@ -1,5 +1,36 @@
 package battleship;
 
 public class Ocean {
+//instance variables
+  private Ship [][] ships = new Ship [10][10];
+  private int shotsFired;
+  private int hitCount;
+  private int shipsSunk;
 
+//constructor
+  public Ocean() {
+    shotsFired = 0;
+    hitCount = 0;
+    shipsSunk = 0;
+
+    for (int row = 0; row < 10; row++;) {
+      for (int col = 0; col < 10; row++;) {
+        ships[row][col] = new EmptySea(); 
+      }
+    }
+  }
+  //methods
+ 
+  /**
+    * Returns the 10x10 array of ships 
+    */
+   Ship [][] getShipArray() {
+     return ships
+       }
+  /**
+    * Places all ten ships randomly on the (initially empty) ocean 
+    */
+  void placeAllShipsRandomly() {
+    
+  
 }

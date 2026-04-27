076389d4b4ef0a03cbb7e3e2002a44df2d3d74ab
kvaitohi <kvaitohi@penncareylaw.upenn.edu>
2026-04-27 14:39:33 -0400

    Update Ocean.java

--- src/battleship/Ocean.java
+++ src/battleship/Ocean.java
@@ -19,14 +19,47 @@ public class Ocean {
       }
     }
   }
+  
   //methods
- 
   /**
     * Returns the 10x10 array of ships 
     */
-   Ship [][] getShipArray() {
-     return ships
+   public Ship [][] getShipArray() {
+     return ships;
        }
+  
+  /**
+    * Returns true if the given location contains a ship, false if it does not  
+    */
+  public boolean isOccupied(int row, int column) {
+    if(ships[row][column] instanceof EmptySea) { //if at the row and column, 
+      return false 
+        } else {
+      return true
+        }
+  }
+  
+  /**
+    * Returns number of ships sunk (in the game)
+    */
+  public int getShipsSunk() {
+    return shipsSunk;  
+  }
+
+  /**
+    * Returns the number of shots fired (in the game) 
+    */
+  public int getShotsFired() {
+    return shotsFired;  
+  }
+
+  /**
+    * Returns number of hits recorded (in the game)
+    */
+  public int getHitCount() {
+    return hitCount; 
+  }
+  
   /**
     * Places all ten ships randomly on the (initially empty) ocean 
     */
